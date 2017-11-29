bd1968619a0fd8a236aff2b9d04a6c8a1b22116d
loreinayg <loreinayg@gmail.com>
2017-11-29 11:38:25 -0600

    To correct up.

--- R/oneDsample.R
+++ R/oneDsample.R
@@ -4,9 +4,11 @@ method<- readline(prompt='Please enter the method you want to use.
 if (abs(integrate(f,lb,ub)$val)>1.001){
   stop("Error: This is not a valid pdf.The area under the function you given should be 1")
 }
+elseif{
 if ('exp' %in% method){
 }
 if ('uni' %in% method){
 }
 if ('stan' %in% method){}
 elseif {stop('You input an invalid method.')}
+}

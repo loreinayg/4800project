0767d2ce36b84b5a2df4e233d507f3cea1b955c9
loreinayg <loreinayg@gmail.com>
2017-11-29 11:33:13 -0600

    This is to check if it is a valid pdf

--- R/oneDsample.R
+++ R/oneDsample.R
@@ -1,6 +1,9 @@
 #oneDsample
 method<- readline(prompt='Please enter the method you want to use.
                   [exponential,uniform,standard only]:')
+if (abs(integrate(f,lb,ub)$val)>1.001){
+  stop("Error: This is not a valid pdf.The area under the function you given should be 1")
+}
 if ('exp' %in% method){
 }
 if ('uni' %in% method){
